ae34994bcfd785b5f015c2430288b43255bd7900
Tomas Crhonek <tomascrhonek@gmail.com>
2025-10-05 15:46:27 +0200

    Viper config manager

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # sha3sum
 Program for summing files using the sha3 standard. Like the unix standard shasum and sha2sum.
-
-It stored sums into PostgreSQL database to easy sum and verify sums over the network.
--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"crypto/sha3"
 	"database/sql"
 	"encoding/hex"
+	"flag"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
+
+	"github.com/spf13/viper"
 
 	_ "github.com/lib/pq"
 )
@@ -17,9 +21,6 @@ type entry struct {
 	hash []byte
 	size int64
 }
-
-const logging = false
-const timming = true
 
 func computeHash(path string, ch chan entry) {
 	fi, err := os.Stat(path)
@@ -38,22 +39,19 @@ func computeHash(path string, ch chan entry) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if logging {
-		log.Println("File size:", n)
+	if int64(n) != size {
+		log.Fatalf("Read %d bytes, expected %d bytes", n, size)
 	}
+
 	hash := sha3.New512()
 	hash.Write(buffer)
 	bs := hash.Sum(nil)
-	if logging {
-		log.Printf("SHA3-512: %x\n", bs)
-	}
 
 	ch <- entry{path: path, hash: bs, size: size}
 }
 
 func dbConnect() *sql.DB {
-	connStr := "user=tomas dbname=tomas host=192.168.42.188 password='pass' sslmode=disable"
+	connStr := "user=" + dbUser + "dbname=" + dbName + "host=" + dbHost + "password='" + dbPassword + "' sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
@@ -62,21 +60,58 @@ func dbConnect() *sql.DB {
 	return db
 }
 
+var dbName string
+var dbUser string
+var dbPassword string
+var dbHost string
+var dbPort int
+
+func config() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		viper.Set("database.host", "localhost")
+		viper.Set("database.port", 5432)
+		viper.Set("database.user", "dbuser")
+		viper.Set("database.password", "dbpassword")
+		viper.Set("database.dbname", "dbname")
+		viper.SafeWriteConfig()
+	}
+
+	dbHost = viper.GetString("database.host")
+	dbPort, err = strconv.Atoi(viper.GetString("database.port"))
+	if err != nil {
+		dbPort = 5432
+	}
+	dbUser = viper.GetString("database.user")
+	dbPassword = viper.GetString("database.password")
+	dbName = viper.GetString("database.dbname")
+}
+
 func main() {
+	logging := flag.Bool("debug", false, "Enable debug")
+	timming := flag.Bool("timming", false, "Enable timming")
+	root := flag.String("root", ".", "Root directory")
+
+	flag.Parse()
+
+	config()
+
 	timeStart := time.Now()
-	if timming {
+	if *timming {
 		log.Println("Timming enabled")
 		log.Println("Start:", timeStart)
 	}
 
-	if logging {
+	if *logging {
 		log.SetOutput(os.Stderr)
 		log.Println("sha3sum started")
 	}
 
-	root := "d:\\Gay\\"
-
-	if logging {
+	if *logging {
 		log.Println("Start:", timeStart)
 	}
 
@@ -85,12 +120,12 @@ func main() {
 
 	ch := make(chan entry, 10000000)
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(*root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if logging {
+		if *logging {
 			log.Println(path)
 		}
 
@@ -117,7 +152,7 @@ func main() {
 		if err != nil {
 			log.Println("Error:", err)
 		}
-		if logging {
+		if *logging {
 			log.Println("Inserted:", e.path)
 		}
 	}
@@ -127,10 +162,9 @@ func main() {
 	}
 
 	timeEnd := time.Now()
-	if timming {
+	if *timming {
 		log.Println("End:", timeEnd)
 		log.Println("Duration:", timeEnd.Sub(timeStart))
 		log.Println("sha3sum finished")
 	}
 }
-
