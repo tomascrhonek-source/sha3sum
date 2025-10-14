b5474a4a13d3064a9c6315f471a2b748b7826a05
Tomas Crhonek <tomascrhonek@gmail.com>
2025-10-14 14:27:21 +0200

    Refactored config

---
+++ config.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/spf13/viper"
+)
+
+type config struct {
+	dbName         string
+	dbUser         string
+	dbPassword     string
+	dbHost         string
+	dbPort         int
+	logging        *bool
+	timming        *bool
+	nodb           *bool
+	maxConnections int
+	root           *string
+}
+
+func defaults() {
+	viper.Set("database.host", "localhost")
+	viper.Set("database.port", 5432)
+	viper.Set("database.user", "dbuser")
+	viper.Set("database.password", "dbpassword")
+	viper.Set("database.dbname", "dbname")
+
+	viper.Set("config.debug", false)
+	viper.Set("config.timming", false)
+	viper.Set("config.root", ".")
+	viper.Set("config.maxconnections", 10)
+	viper.Set("config.nodb", false)
+
+	viper.SafeWriteConfig()
+}
+
+func configure() config {
+	viper.SetConfigName("sha3sum")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("$HOME/.config/")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		defaults()
+	}
+
+	cfg := config{}
+	cfg.dbHost = viper.GetString("database.host")
+	cfg.dbPort, err = strconv.Atoi(viper.GetString("database.port"))
+	if err != nil {
+		cfg.dbPort = 5432
+	}
+	cfg.dbUser = viper.GetString("database.user")
+	cfg.dbPassword = viper.GetString("database.password")
+	cfg.dbName = viper.GetString("database.dbname")
+
+	cfg.logging = new(bool)
+	cfg.timming = new(bool)
+	cfg.nodb = new(bool)
+	cfg.root = new(string)
+	cfg.maxConnections, err = strconv.Atoi(viper.GetString("config.maxconnections"))
+	if err != nil {
+		cfg.maxConnections = 10
+	}
+	*cfg.logging = viper.GetBool("config.debug")
+	*cfg.timming = viper.GetBool("config.timming")
+	*cfg.nodb = viper.GetBool("config.nodb")
+	*cfg.root = viper.GetString("config.root")
+
+	return cfg
+}
--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"sync"
 	"time"
 
-	"github.com/spf13/viper"
+	_ "net/http/pprof"
 
 	_ "github.com/lib/pq"
 )
@@ -25,71 +24,10 @@ type entry struct {
 	size int64
 }
 
-type config struct {
-	dbName         string
-	dbUser         string
-	dbPassword     string
-	dbHost         string
-	dbPort         int
-	logging        *bool
-	timming        *bool
-	nodb           *bool
-	maxConnections int
-	root           *string
-}
-
 var pool sync.Map
 
 func init() {
 	pool = sync.Map{}
-}
-
-func configure() config {
-	viper.SetConfigName("sha3sum")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("$HOME/.config/")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		viper.Set("database.host", "localhost")
-		viper.Set("database.port", 5432)
-		viper.Set("database.user", "dbuser")
-		viper.Set("database.password", "dbpassword")
-		viper.Set("database.dbname", "dbname")
-
-		viper.Set("config.debug", false)
-		viper.Set("config.timming", false)
-		viper.Set("config.root", ".")
-		viper.Set("config.maxconnections", 10)
-		viper.Set("config.nodb", false)
-
-		viper.SafeWriteConfig()
-	}
-
-	cfg := config{}
-	cfg.dbHost = viper.GetString("database.host")
-	cfg.dbPort, err = strconv.Atoi(viper.GetString("database.port"))
-	if err != nil {
-		cfg.dbPort = 5432
-	}
-	cfg.dbUser = viper.GetString("database.user")
-	cfg.dbPassword = viper.GetString("database.password")
-	cfg.dbName = viper.GetString("database.dbname")
-
-	cfg.logging = new(bool)
-	cfg.timming = new(bool)
-	cfg.nodb = new(bool)
-	cfg.root = new(string)
-	cfg.maxConnections, err = strconv.Atoi(viper.GetString("config.maxconnections"))
-	if err != nil {
-		cfg.maxConnections = 10
-	}
-	*cfg.logging = viper.GetBool("config.debug")
-	*cfg.timming = viper.GetBool("config.timming")
-	*cfg.nodb = viper.GetBool("config.nodb")
-	*cfg.root = viper.GetString("config.root")
-
-	return cfg
 }
 
 func main() {
